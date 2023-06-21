b7bdd2758faff87ee80b4d65d5f26e3a858f2492
Gage <77414756+Royuin@users.noreply.github.com>
2023-06-21 07:55:52 -0500

    Made Header component display players current score and highScore

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,9 @@
-export default function Header () {
+export default function Header ({score, highScore}) {
   return (
     <header>
       <h1>Futurama Memory Card </h1>
+      <h2>Score: {score} </h2>
+      <h2>High Score: {highScore} </h2>
     </header>
   )
 }
