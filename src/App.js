a04255d1490ac01f36feadfe6e32b15a04eea4d9
Gage <77414756+Royuin@users.noreply.github.com>
2023-06-21 07:55:09 -0500

    Added score and highScore state values for keeping track of users best score and current score as they are playing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import { useState } from 'react';
 import './App.css';
-import Header from './components/Header'
+import Header from './components/Header';
 
 function App() {
+  const [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+
   return (
     <div className="App">
-      <Header />
+      <Header score={score} highScore={highScore} />
     </div>
   );
 }
