907bc2db7d0587f0e820e351f25b921ec44b4875
Gage <77414756+Royuin@users.noreply.github.com>
2023-06-22 20:54:30 -0500

    Imported images for characters and made characters array in state to store all of them

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
 import { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
+import bender from './assets/bender.webp';
+import fry from './assets/fry.webp';
+import leela from './assets/leela.webp';
+import amy from './assets/amy.webp';
+import hermes from './assets/hermes.webp';
+import zoidberg from './assets/zoidberg.webp';
+import farnsworth from './assets/farnsworth.webp';
+import scruffy from './assets/scruffy.webp';
+import uniqid from 'uniqid'; 
 
-function App() {
+export default function App() {
   const [score, setScore] = useState(0);
   const [highScore, setHighScore] = useState(0);
+  const [characters, setCharacters] = useState([
+    {link: bender,
+      name: 'Bender',
+    id: uniqid()},
+    {link: fry,
+      name: 'Fry',
+    id: uniqid()},
+    {link: leela,
+      name: 'Leela',
+    id: uniqid()},
+    {link: amy,
+      name: 'Amy',
+    id: uniqid()},
+    {link: hermes,
+      name: 'Hermes',
+    id: uniqid()},
+    {link: zoidberg,
+      name: 'Zoidberg',
+    id: uniqid()},
+    {link: farnsworth,
+      name: 'Farnsworth',
+    id: uniqid()},
+    {link: scruffy,
+      name: 'Scruffy',
+    id: uniqid()}
+    ]);
 
   return (
     <div className="App">
@@ -12,5 +47,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
