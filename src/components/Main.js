b533a1587db6ae6ba23fcba8f9a9781494eb7076
Gage <77414756+Royuin@users.noreply.github.com>
2023-06-23 20:22:53 -0500

    Made renderCards function that takes all characters and returns each one with its own image and name as a li item all wrapped in a ul

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,3 +1,23 @@
-export default function Main () {
+export default function Main ({characters}) {
+
+  function renderCards(){
+    const cardItems = characters.map((character) => {
+      return (
+        <li className='card' key={character.id}>
+          <img src={character.link} alt={character.name +  ' from Futurama standing'} />
+          <p>{character.name}</p>
+        </li>
+      );
+    });
+
+    return <ul>{cardItems}</ul>
+  }
+
+
+  return (
+    <main>
+      {renderCards()}
+    </main>
+  )
 
 }
