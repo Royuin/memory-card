32be8fd5377ec3913160cfc93eb338d5c15821d3
Gage <77414756+Royuin@users.noreply.github.com>
2023-06-24 08:01:52 -0500

    Made each character li item call shuffle and checkIfInMemory functions

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,22 @@
-export default function Main ({characters}) {
+export default function Main ({characters, shuffle, checkIfInMemory}) {
 
   function renderCards(){
     const cardItems = characters.map((character) => {
+      const id =character.id;
       return (
-        <li className='card' key={character.id}>
-          <img src={character.link} alt={character.name +  ' from Futurama standing'} />
-          <p>{character.name}</p>
+        <li onClick={() => {
+        shuffle();
+        checkIfInMemory(id);
+        }}
+          className='card' key={character.id} >
+          <img  src={character.link} alt={character.name +  ' from Futurama standing'}  />
+          <p >{character.name}</p>
         </li>
       );
     });
 
     return <ul>{cardItems}</ul>
   }
-
 
   return (
     <main>
